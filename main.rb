f6cd6f1d91ad5f64a5f35aaaacc1e00d77b153a8
akicho8 <akicho8@gmail.com>
2017-12-27 13:29:53 +0900

    2ch棋譜の対局者名の取り込み

--- main.rb
+++ main.rb
@@ -1,39 +1,99 @@
 require "kconv"
 require "pathname"
 require "open-uri"
+require "natto"
 
-shogi_vje_txt_url = "https://raw.githubusercontent.com/knu/imedic-shogi/master/shogi.vje.txt"
+class App
+  def run
+    @out = []
+    @out << ";; -*- coding: euc-jp-unix -*-"
+    @out << ";; (skk-restart)"
+    @out << ";;"
+    @out << ";; okuri-ari entries."
+    @out << ";; okuri-nasi entries."
 
-file = Pathname("shogi.vje.txt")
+    @rows = []
 
-unless file.exist?
-  file.write(open(shogi_vje_txt_url, &:read))
-end
+    shogi_vje_txt_import
+    nichan_kifu_import
 
-body = file.read.toutf8
+    @out += @rows
 
-out = []
-out << ";; -*- coding: euc-jp-unix -*-"
-out << ";; (skk-restart)"
-out << ";;"
-out << ";; okuri-ari entries."
-out << ";; okuri-nasi entries."
+    str = @out.join("\n") + "\n"
+    file = Pathname("SKK-JISYO.shogi.dic")
+    file.write(str.toeuc)
+    p @out.count
+    puts file
+  end
 
-body.lines.collect(&:strip).each do |line|
-  next if line == ""
-  if line.match(/^#/)
-    out << ";; #{line}"
-  else
-    # "あい\t合\t【名サ】"
-    yomi, kanji, syurui = line.split(/\t+/)
-    if yomi.match?(/[[:digit:]]{2}/)
-      p [:skip, yomi]
-      next
+  def shogi_vje_txt_import
+    shogi_vje_txt_url = "https://raw.githubusercontent.com/knu/imedic-shogi/master/shogi.vje.txt"
+
+    file = Pathname("shogi.vje.txt")
+
+    unless file.exist?
+      file.write(open(shogi_vje_txt_url, &:read))
     end
-    yomi = yomi.tr("０-９", "0-9")
-    out << "#{yomi} /#{kanji}/"
+
+    body = file.read.toutf8
+
+    body.lines.collect(&:strip).each do |line|
+      next if line == ""
+      if line.match(/^#/)
+        @out << ";; #{line}"
+      else
+        # "あい\t合\t【名サ】"
+        yomi, kanji, syurui = line.split(/\t+/)
+
+        # DDSKKの場合、将棋の座標の入力に特化した仕組みがあるため省く
+        if yomi.match?(/[[:digit:]]{2}/)
+          # p [:skip, yomi]
+          next
+        end
+
+        yomi = yomi.tr("０-９", "0-9") # だいたいのSKKユーザーは全角数字を入力するのが難しいため
+
+        @rows << "#{yomi} /#{kanji}/"
+      end
+    end
+  end
+
+  def nichan_kifu_import
+    other = Pathname("2ch棋譜名前一覧_特殊.txt").readlines.inject({}) do |a, e|
+      kanji, yomi = e.split(/\s+/)
+      a.merge(kanji => yomi)
+    end
+
+    nm = Natto::MeCab.new('-F%m\t%f[0]\t%f[7]')
+    Pathname("2ch棋譜名前一覧.txt").readlines.each do |e|
+      e = e.strip
+      if e.match?(/\p{ASCII}/)
+      else
+        begin
+          yomi_list = []
+          enum = nm.enum_parse(e)
+          enum.each do |e|
+            if e.is_eos?
+              break
+            end
+            kanji, syurui, yomi = e.feature.split(/\t/)
+            yomi = NKF.nkf("-w --hiragana", yomi)
+            yomi_list << yomi
+          end
+          # p [yomi_list.join, e]
+          @rows << "#{yomi_list.join} /#{e}/"
+        rescue Natto::MeCabError
+          if yomi = other[e]
+            p ["読み方補完", e, yomi]
+            @rows << "#{yomi} /#{e}/"
+          else
+            @rows << ";; 読み方不明 /#{e}/ "
+            p ["読み方不明", e]
+          end
+        end
+      end
+    end
   end
 end
-str = out.join("\n") + "\n"
-puts str
-Pathname("SKK-JISYO.shogi.dic").write(str.toeuc)
+
+App.new.run
